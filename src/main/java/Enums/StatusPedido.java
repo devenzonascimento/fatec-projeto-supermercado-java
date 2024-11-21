f356217bbb6f052012cb62cae3c0fcfc2a1d395f
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-11-20 22:57:33 -0300

    add Enums

---
+++ src/main/java/Enums/StatusPedido.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public enum StatusPedido {
+    PENDENTE,
+    ENTREGUE,
+    CANCELADO
+}
---
+++ src/main/java/Enums/TipoPagamento.java
@@ -0,0 +1,6 @@
+package Enums;
+
+public enum TipoPagamento {
+    ENTRADA,
+    SAIDA
+}
