ce6d0107daaec0de4d59bb6cb008e998836e2a8a
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-11-21 20:42:02 -0300

    criado classe abstrata Contato para generalizar o Fornecedor e Cliente

---
+++ src/main/java/Entidade/Contato.java
@@ -0,0 +1,63 @@
+package Entidade;
+
+public abstract class Contato {
+    private long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+    public Contato() {}
+
+    public Contato(String nome, String telefone, String email, String endereco) {
+        setNome(nome);
+        setTelefone(telefone);
+        setEmail(email);
+        setEndereco(endereco);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
--- src/main/java/Entidade/Fornecedor.java
+++ src/main/java/Entidade/Fornecedor.java
@@ -1,37 +1,18 @@
 package Entidade;
 
-public class Fornecedor {
-    private long id;
-    private String nome;
+public class Fornecedor extends Contato {
+
     private String cnpj;
-    private String telefone;
-    private String email;
-    private String endereco;
 
     public Fornecedor() {}
 
     public Fornecedor(String nome, String cnpj, String telefone, String email, String endereco) {
+        super();
         setNome(nome);
         setCnpj(cnpj);
         setTelefone(telefone);
         setEmail(email);
         setEndereco(endereco);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getCnpj() {
@@ -40,34 +21,5 @@ public class Fornecedor {
 
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    @Override
-    public String toString() {
-        return this.nome;
     }
 }
