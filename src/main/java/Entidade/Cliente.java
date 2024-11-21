99004d1b1d509356046960c7efbf9d086947e46c
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-11-21 20:42:17 -0300

    Adicionado a Classe Cliente

---
+++ src/main/java/Entidade/Cliente.java
@@ -0,0 +1,23 @@
+package Entidade;
+
+public class Cliente extends Contato {
+    private String cpf;
+    public Cliente() {}
+
+    public Cliente(String nome, String cpf, String telefone, String email, String endereco) {
+        super();
+        setNome(nome);
+        setCpf(cpf);
+        setTelefone(telefone);
+        setEmail(email);
+        setEndereco(endereco);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
