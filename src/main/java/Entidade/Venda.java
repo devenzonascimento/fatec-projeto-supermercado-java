31d28b9c758d0d24b2fecb4aef35cd159898c8cd
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-11-22 01:40:14 -0300

    adicionada a classe Venda

---
+++ src/main/java/Entidade/Venda.java
@@ -0,0 +1,63 @@
+package Entidade;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Venda {
+    private long id;
+    private Date data;
+    private Cliente cliente;
+    private Pagamento pagamento;
+    private ArrayList<ItemProduto> itens;
+
+    public Venda() {
+    }
+
+    public Venda(long id, Date data, Cliente cliente, Pagamento pagamento, ArrayList<ItemProduto> itens) {
+        setId(id);
+        setData(data);
+        setCliente(cliente);
+        setPagamento(pagamento);
+        setItens(itens);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public ArrayList<ItemProduto> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<ItemProduto> itens) {
+        this.itens = itens;
+    }
+}
