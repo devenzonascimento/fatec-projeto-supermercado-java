5372d8db361da981cc2a727aaa74331e7805a1ec
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2024-11-24 00:05:31 -0300

    adicionado logica para centralizar o interface grafico na janela

--- src/main/java/Supermercado.java
+++ src/main/java/Supermercado.java
@@ -1,13 +1,23 @@
 import Tela.*;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Supermercado {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Sistema de Gerenciamento de Supermercado");
 
+    private static final int LARGURA_DA_TELA = 1000;
+    private static final int ALTURA_DA_TELA = 700;
+
+    public static void main(String[] args) {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension tamanhoDaJanela = kit.getScreenSize();
+
+        Point localizacaoCentralizada = new Point((tamanhoDaJanela.width / 2) - LARGURA_DA_TELA / 2, (tamanhoDaJanela.height / 2) - ALTURA_DA_TELA / 2);
+
+        JFrame frame = new JFrame("Sistema de Gerenciamento de Supermercado");
+        frame.setSize(LARGURA_DA_TELA, ALTURA_DA_TELA);
+        frame.setLocation(localizacaoCentralizada);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 700);
 
         JTabbedPane secoes = new JTabbedPane();
 
